5cc1230023d9d3ba666b0deed8c7ded7eec0a6d5
ismaelmartinses59-hash <ismaelmartinses59@gmail.com>
2026-05-26 20:45:49 -0300

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -2,23 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-if (import.meta.env.PROD) {
-  import("virtual:pwa-register").then(({ registerSW }) => {
-    registerSW({
-      immediate: true,
-      onNeedRefresh() {
-        window.location.reload();
-      },
-      onRegisteredSW(_swUrl, registration) {
-        if (!registration) return;
-        setInterval(() => {
-          registration.update().catch(() => {});
-        }, 60 * 1000);
-      },
-    });
-  }).catch(() => {});
-}
-
 const root = createRoot(document.getElementById("root")!);
 root.render(<App />);
 
